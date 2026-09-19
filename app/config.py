c96ac00c3430874d394593b1431f0b93fb2e6857
dawid <praktykant31415@gmail.com>
2026-09-19 07:34:22 +0200

    feat: new things

--- app/config.py
+++ app/config.py
@@ -2,11 +2,12 @@ from pydantic_settings import BaseSettings,SettingsConfigDict
 class Settings(BaseSettings):
     # These will automatically read from environment variables 
     # whether they come from a local .env file OR from Kubernetes!
+    CLERK_PUBLIC_KEY: str
+    CLERK_SECRET_KEY: str
     DATABASE_URL: str
     OPENROUTER_API_KEY: str
     VENUE_NAME: str = "Galeria Katowicka"
     MODEL: str = "openai/gpt-4o-mini"
-    # Updated to Pydantic V2 SettingsConfigDict syntax
     model_config = SettingsConfigDict(
         env_file="../configs/.env", 
         env_file_encoding="utf-8"
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 #routes.py
 from database import AsyncSessionLocal,get_db,AsyncSession
-from schemas import ChatHistory,Book,Author,BookNotFound,Message
+from schemas import ChatHistory,Message
+from auth import get_current_user
 from crud import CRUD
 from sse_starlette.sse import EventSourceResponse
 from fastapi import APIRouter,FastAPI,HTTPException,status,Depends
@@ -18,3 +19,9 @@ description="--")
 async def stream_chat(chat_data: ChatHistory) -> EventSourceResponse:
     #! in prod change debug_print_response to llm_call
     return EventSourceResponse(full_response(chat_data))
+
+@router.get("/test/endpoint")
+async def show_user_id(
+    db : AsyncSession,
+    user_payload: dict = Depends(get_current_user)
+)
